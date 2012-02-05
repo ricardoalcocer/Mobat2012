0bb91b1c301dbd5862c9aea117efc48aff6d0f37
Ricardo Alcocer <ralcocer@gettainoapp.com>
2012-02-05 11:29:27 -0400

    License headers added to modules

--- Resources/modules/http.js
+++ Resources/modules/http.js
@@ -1,17 +1,27 @@
-/**
- * Titanium HTTP GET Wrapper Class
- *
- * To use this class should use a block of code like this:
- *
- * Ti.include('../libs/http.js');
- * http.get('<<insert URL to call>>',function(htmlSource){
- * 	// Insert code that relies on the output of the HTTP Call
- *             // Output is in the htmlSource variable
- * });
- *
- * @author Ricardo ALcocer
- * @version 1.0
- *
+/*
+ * This file is part of MOBAT.
+ * 
+ * MOBAT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ * 
+ * MOBAT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ * Name: 			http.js
+ * Description: 	
+ * Date:			
+ * Author:			
+ * Company:			
+ * Version:			
+ * 
+ * Notes:
+ * 
  * 
  */
 var http = function(){
