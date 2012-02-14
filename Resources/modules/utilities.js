f0d2cf40d6a9eae344b44dcf7e56b35a76ad6770
Ricardo Alcocer <ralcocer@gettainoapp.com>
2012-02-13 23:49:59 -0400

    Added function validateemailaddress to utilities

--- Resources/modules/utilities.js
+++ Resources/modules/utilities.js
@@ -28,4 +28,13 @@
 		str=str.replace('&lt;/del&gt;','');
 		return str;
 	}
+	
+	app.modules.utilities.validateEmail=function(emailAddress){
+		var rt = false;
+		var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;    
+   		if(reg.test(emailAddress) == true) {
+   			rt = true;
+   		}
+   		return rt;
+	}
 })()
